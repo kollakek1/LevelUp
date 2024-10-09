f2b747a49265b1b1afb553d672bc20e0d820c67f
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-10-09 21:39:20 +0800

    first spet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
+
+import auth from 'auth-astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), auth()],
+  output: 'server',
+  adapter: vercel()
+});
