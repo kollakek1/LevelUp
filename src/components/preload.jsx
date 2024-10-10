9468a3f6b2b088a94414b829775ac86a2a9ae02a
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-10-10 22:40:49 +0800

    preload

--- src/components/preload.jsx
+++ src/components/preload.jsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from "react";
 
 export default function Preload() {
-    const [loading, setLoading] = useState(true);
+    const [load, setLoad] = useState(true);
 
     useEffect(() => {
-        const handleLoad = () => setLoading(false);
-        window.addEventListener("load", handleLoad);
+        const handleLoad = () => setLoad(false);
+        
+        if (document.readyState === "complete") {
+            setLoad(false);
+        } else {
+            window.addEventListener("load", handleLoad);
+        }
 
         return () => window.removeEventListener("load", handleLoad);
+    }, []);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setLoad(false), 3000);
+
+        return () => clearTimeout(timeout);
     }, []);
 
     return (
         <div
             className={`w-screen h-screen flex items-center justify-center bg-base-100 fixed transition-opacity duration-500 ${
-                !loading ? "opacity-0 pointer-events-none -z-50" : "z-50"
+                !load ? "opacity-0 pointer-events-none" : "z-50"
             }`}
         >
             <div className="text-center">
