e39170ef4d6ae9e62f15c9c66d65fd7f74095e9c
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-10-14 21:46:08 +0800

    Update chat.jsx

--- src/components/app/components/chat.jsx
+++ src/components/app/components/chat.jsx
@@ -1,10 +1,20 @@
 import { MdArrowBackIos, MdOutlineAddCircle, MdSend } from "react-icons/md";
 
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 export default function Chat({ user, active }) {
 
     const navigate = useNavigate();
+
+    const [content, setContent] = useState('');
+
+    const handleInput = (e) => {
+        const text = e.target.innerText.trim();
+        setContent(text);
+    };
+
+    const isEmpty = content.length === 0;
 
     return (
         <div className={`transition-all duration-500 h-screen w-full max-lg:bg-base-100 absolute lg:p-4 ${active ? "translate-y-0" : "max-lg:translate-x-full lg:translate-y-full pointer-events-none"}`}>
@@ -16,12 +26,10 @@ export default function Chat({ user, active }) {
 
                 <div className="textarea items-center gap-2 absolute bottom-0 bg-base-200 w-full p-2 flex justify-between max-lg:rounded-none text-lg">
                     <MdOutlineAddCircle className="w-7 h-7 duration-150 hover:text-primary cursor-pointer"/>
-                    <div className="max-lg:bg-base-300 rounded-btn p-1 w-full flex items-center">
-                        <div contentEditable="true" className="w-full block min-h-7 max-h-16 overflow-y-auto outline-none box-border break-words">
-                            
-                        </div>
+                    <div contentEditable="plaintext-only" className="w-full min-h-7 max-h-16 max-lg:bg-base-300 rounded-btn overflow-y-auto outline-none box-border break-words" onInput={handleInput}>
+                        
                     </div>
-                    <MdSend className="w-7 h-7 duration-150 hover:text-primary cursor-pointer"/>
+                    <MdSend className={`w-7 h-7 duration-150 hover:text-primary cursor-pointer ${isEmpty ? "w-0 h-0" : ""}`}/>
                 </div>
             </div>
         </div>
