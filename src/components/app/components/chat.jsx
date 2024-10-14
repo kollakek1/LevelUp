178128f64cd8bdf8e1affee009a2b0b9ff7be84d
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-10-14 19:53:34 +0800

    chat input update

--- src/components/app/components/chat.jsx
+++ src/components/app/components/chat.jsx
@@ -1,3 +1,5 @@
+import { MdArrowBackIos, MdOutlineAddCircle } from "react-icons/md";
+
 import { useNavigate } from "react-router-dom";
 
 export default function Chat({ user, active }) {
@@ -5,13 +7,18 @@ export default function Chat({ user, active }) {
     const navigate = useNavigate();
 
     return (
-        <div className={`transition-all duration-500 h-screen w-full max-lg:bg-base-100 absolute p-4 ${active ? "translate-y-0" : "translate-y-full pointer-events-none"}`}>
+        <div className={`transition-all duration-500 h-screen w-full max-lg:bg-base-100 absolute lg:p-4 ${active ? "translate-y-0" : "max-lg:translate-x-full lg:translate-y-full pointer-events-none"}`}>
             <div className="w-full h-full relative">
-                <div className="w-full bg-base-200 rounded-box h-14">
-
+                <div className="w-full bg-base-200 lg:rounded-box relative p-3 flex items-center justify-between">
+                    <MdArrowBackIos className="w-7 h-7 lg:hidden" onClick={() => navigate("/app/@me")}/>
+                    <h2 className="text-sm font-medium">Nigger</h2>
                 </div>
 
-                <div className="absolute bottom-0 bg-base-200 w-full h-14 p-2 flex justify-between rounded-btn">
+                <div className="input max-lg:!outline-none max-lg:border-0 items-center gap-2 absolute bottom-0 bg-base-200 w-full p-2 flex justify-between max-lg:rounded-none h-16 text-lg">
+                    <MdOutlineAddCircle className="w-7 h-7"/>
+                    <div className="grow max-lg:bg-base-300 rounded-btn p-1 w-full">
+                        <input type="text" className="w-full" placeholder="Написать @nigger" />
+                    </div>
                 </div>
             </div>
         </div>
